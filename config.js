ab7e76377c308d04b66cfbf0bf9146090f100262
brentbillington-ship-it <brent.billington@gmail.com>
2026-03-10 09:13:01 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ─── Configuration ───
 // Paste your Google Apps Script deployment URL below
 const CONFIG = {
-  SHEETS_API_URL: 'YOUR_APPS_SCRIPT_URL_HERE',
+  SHEETS_API_URL: 'https://script.google.com/macros/s/AKfycbzOPQdt1wBuYGCv_sKTG-otW0jayKr3H9IFG6qGaoZy1qb_ZulhmNYFBlukjvJEa6yawA/exec',
   ADMIN_PASSWORD: 'chaka2026',
   DEFAULT_CITY: 'Coppell, TX 75019',
   MAP_CENTER: [32.972, -96.978],
